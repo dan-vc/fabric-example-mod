4e69c55dd86b1006b33b2d40467e5ce3810e6d55
Daniel Villafranqui <danedo20472@gmail.com>
2025-11-24 20:51:37 -0500

    first commit

---
+++ src/main/java/net/daner/examplemod/Examplemod.java
@@ -0,0 +1,17 @@
+package net.daner.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Examplemod implements ModInitializer {
+	public static final String MOD_ID = "example-mod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
